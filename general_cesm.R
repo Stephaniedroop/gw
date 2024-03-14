505c44fcf75d681591a5697b762b8b06ab2962d1
Stephanie Droop <55804396+Stephaniedroop@users.noreply.github.com>
2024-03-14 10:41:05 +0000

    more general cesm function

--- general_cesm.R
+++ general_cesm.R
@@ -46,19 +46,7 @@ generic_cesm <- function(prior, structure) {
   # ... with variables as the column names
   colnames(df) <- causes
   worlds <- nrow(df)
-  # Next 10 lines put a column for the prior of each combination of variable settings
-  # Start by taking a copy in same shape
-  df2 <- as.matrix(df)
-  # Replace every cell with the relevant indexed edge strength from the prior object
-  for (k in 1:worlds){
-    for (cause in causes) {
-      a <- prior[cause,df[k,cause]+1] # It needs the '+1' because r indexes from 1 not 0
-      df2[k,cause] <- a # ((then sometimes #*df[k,cause] if do at same time as structure but change later if need))
-    }
-  }
-  # For each row of df, the prior is now the product of the same row of df2
-  df$Pr <- rowprods(df2)
-  df$pOutcome <- 1 # Deterministic for now; will change later
+  
   # Next section calculates EFFECT (E) depending on whether structure is disjunctive or conjunctive
   if (structure=="conjunctive") { 
     df$E <- as.numeric((df[1] & df[2]) & (df[3] & df[4])) # Let's handle causes!=4 later
@@ -66,12 +54,26 @@ generic_cesm <- function(prior, structure) {
   if (structure=="disjunctive") { 
     df$E <- as.numeric((df[1] & df[2]) | (df[3] & df[4])) 
   }
+  # Can replace with this - if rename 
+  # df$effect <- max( c(min(c1,e1), min(c2,e2), min(c3, e3), min(c2*c3, e23))) # BUT SAME PROBLEM - HOW TO AUTOMATICALLY DEAL WITH ANY NUMBER OF CAUSES?
+  
+  
+  # NEW BIT USING NEIL'S
+  # NOISY-OR for probabilistic behaviour - this always takes p(p==1) but when cause==0 the term becomes 0 anyway so doesn't matter
+  df$test <- as.numeric(unlist(1 - ((1 - prior[1,2] * df[1]) * (1 - prior[2,2] * df[2]) * (1 - prior[3,2] * df[3]) * (1 - prior[4,2] * df[4]))))
+  
+  # FIND WAY TO RENAME because can't find way to get rid of out out.attrs
+  #names(df)[6] <- "test"
+  #names(df)[names(df) == 'test$p_A'] <- 'test'
+  
   # Then loop to calculate cfs and assign causal responsibility
   # Loop through possible world settings 
   for (c_ix in 1:worlds)
   {
     # The current case
     case <- df[c_ix,]
+    
+    # Below here was working end Feb 2024
     # Make an empty df to put the generated counterfactual settings in. 
     cfs <- data.frame(matrix(NA, nrow = N_cf, ncol = ncol(df))) 
     colnames(cfs) <- colnames(df)
@@ -107,7 +109,9 @@ generic_cesm <- function(prior, structure) {
     #est <- rep(NA, n_causes)
     for (cause in 1:n_causes)
     {
-      cor_sizes[cause] <- cor(cfs[[causes[cause]]], cfs$Match) # * (c(-1,1)[as.numeric(case[[causes[cause]]])]) commented out bit fixes negative but not working here yet
+      # Calculate correlation - commented part sets correlation negative when cause not present, and positive when present
+      cor_sizes[cause] <- cor(cfs[[causes[cause]]], cfs$Match) * (c(-1,1)[as.numeric(case[[causes[cause]]])]) 
+      # FIX REPLACEMENT LENGTH ZERO ERROR
       # Will be nice to do regression across each - do we need to count the results though?
       #est[cause] <- glmer(Match ~ 1 + cause, data = cfs, family = "binomial")
     }
