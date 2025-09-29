dcdffe3e783f8ce0769d2ab6660468aecdf874a8
Stephanie Droop <55804396+Stephaniedroop@users.noreply.github.com>
2025-09-29 09:32:29 +0100

    latest mod

--- Exp1Prediction/Model/Scripts/03processModel.r
+++ Exp1Prediction/Model/Scripts/03processModel.r
@@ -6,7 +6,7 @@ library(here)
 source(here('Exp1Prediction', 'Model', 'Scripts', 'modelUtils.R'))
 
 # Load fitted models from `01findModel.R`: 2 dfs of 59049 rows of 13 vars each: path and destination
-load(here('Exp1Prediction', 'Model', 'Data', 'fitted.rdata'))
+load(here('Exp1Prediction', 'Model', 'Data', 'fitted.rda'))
 
 # Count number of edges in each structure
 fitted_path_mods$n_edge <- rowSums(structures!=0)
@@ -17,17 +17,17 @@ complexity_penalisation <- 0.002 # This was picked arbitrarily:
 #TO DO  justify by plotting how stable it is at different vals of complexity? TO DO free parameter hand fit.
 
 # Find which model has the lowest KL divergence - that's the one we want!
-which.min(fitted_path_mods$kl) # 31717 for first td but redone as 32204
+which.min(fitted_path_mods$kl) # 31717 for first td but redone as 32204 - no - 25874
 
 # Make the KL bigger for each edge so to penalise complexity and so avoid saturated fully connected model
-bpix <- which.min(fitted_path_mods$kl + fitted_path_mods$n_edge*complexity_penalisation) # 29291
-bdix <- which.min(fitted_destination_mods$kl + fitted_destination_mods$n_edge*complexity_penalisation) # 32793
+bpix <- which.min(fitted_path_mods$kl + fitted_path_mods$n_edge*complexity_penalisation) # 22967
+bdix <- which.min(fitted_destination_mods$kl + fitted_destination_mods$n_edge*complexity_penalisation) # 58713
 
 # Find the best fitting structures and their parameters
-best_path <- structures[bpix,] # 00100-10000
+best_path <- structures[bpix,] # 00100-10000 - no - 01000000-10
 best_path_params <- fitted_path_mods[bpix,]
 
-best_destination <- structures[bdix,] # 10011-11100
+best_destination <- structures[bdix,] # 10011-11100 - no - 1001001111
 best_destination_params <- fitted_destination_mods[bdix,]
 
 # Create the best fitting models for display
